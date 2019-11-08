206ec1a7757cdc62c19251ee70cca312bed7898a
manuelrucci <rucci@localhost.com>
2019-11-08 12:38:34 +0100

    added QR CODE

--- qr_reader_ros_ws/scripts/launch_full_task2.sh
+++ qr_reader_ros_ws/scripts/launch_full_task2.sh
@@ -8,14 +8,14 @@ exec bash\""  \
 sleep 4 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'cd /root/catkin_ws && source /opt/ros/melodic/setup.bash && catkin_make && source /root/catkin_ws/devel/setup.bash && roscore';
 exec bash\""  \
 --tab --title "GAZEBO PLUGIN BUILD" --command "bash -c \"
-sleep 15 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'cd /root/catkin_ws/src/gazebo_models_pkg/ && mkdir -p build && cd build && cmake .. && make';
+sleep 15 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && cd /root/catkin_ws/src/gazebo_models_pkg/ && mkdir -p build && cd build && cmake .. && make';
 exec bash\""  \
 --tab --title "GAZEBO SIMULATION"	--command "bash -c \"
-sleep 25 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch /root/catkin_ws/src/gazebo_models_pkg/my_world.launch --wait world_name:=qr_world';
+sleep 35 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch /root/catkin_ws/src/gazebo_models_pkg/my_world.launch --wait world_name:=qr_world';
 exec bash\""  \
 --tab --title "QR_DETECTOR"	--command "bash -c \"
-sleep 25 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch qr_detector_pkg qr_detector_cams.launch --wait';
+sleep 35 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch qr_detector_pkg qr_detector_cams.launch --wait';
 exec bash\""  \
 --tab --title "WEB_VIDEO_SERVER" --command "bash -c \"
-sleep 25 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch web_video_server web_video_server.launch --wait';
+sleep 35 && docker exec -it ros_melodic_desktop_full_task_2 /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch web_video_server web_video_server.launch --wait';
 exec bash\"" & 
